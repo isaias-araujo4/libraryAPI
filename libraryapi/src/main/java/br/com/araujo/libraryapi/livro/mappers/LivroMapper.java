14f630ff01b16e7b7f2534db9b3de75a5cbb85fe
Isaias Araujo <araujoisaias58@gmail.com>
2026-09-18 16:44:17 -0300

    correção mapper livros

--- libraryapi/src/main/java/br/com/araujo/libraryapi/livro/mappers/LivroMapper.java
+++ libraryapi/src/main/java/br/com/araujo/libraryapi/livro/mappers/LivroMapper.java
@@ -1,22 +1,17 @@
 package br.com.araujo.libraryapi.livro.mappers;
 
 import br.com.araujo.libraryapi.autor.mappers.AutorMapper;
-import br.com.araujo.libraryapi.autor.repository.AutorRepository;
 import br.com.araujo.libraryapi.livro.model.Livro;
 import br.com.araujo.libraryapi.livro.model.dto.LivroRequestDTO;
 import br.com.araujo.libraryapi.livro.model.dto.LivroResponseDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring", uses = AutorMapper.class)
-public abstract class LivroMapper {
+public interface LivroMapper {
 
-    @Autowired
-    AutorRepository autorRepository;
+    @Mapping(target = "autor", ignore = true)
+    Livro toEntity(LivroRequestDTO livroRequestDTO);
 
-    @Mapping(target = "autor", expression = "java(autorRepository.findById(cadastroLivroDTO.idAutor()).orElse(null))")
-    public abstract Livro toEntity(LivroRequestDTO livroRequestDTO);
-
-    public abstract LivroResponseDTO toLivroDTO(Livro livro);
+    LivroResponseDTO toLivroDTO(Livro livro);
 }
